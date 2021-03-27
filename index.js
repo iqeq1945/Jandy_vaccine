c7c9a47ae90c7f08ce79946d3bec8083ea615a26
Ungs <50164778+iqeq1945@users.noreply.github.com>
2021-03-27 20:52:29 +0900

    # Throw Error
    Catch the Error
    [Bug]

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.set('views engine', 'ejs');
-app.use(bodyParser().json);
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 
--- routes/vaccine.js
+++ routes/vaccine.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var request = request(require);
+//var request = require('require');
+
+//const url = 'http://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=1&perPage=10';
+//const key = 'tNoct9hJ2VkbcD8RcdNsB8eVlJRGVALc80SKwRJu0jikOI5VbSTAXim5O07jvbEk7wTGmescTrqLsRd5Xz6WOw%3D%3D';
+
+
 router.get('/', function(req, res, next){
+    //console.log(url,key);
     res.render('index');
-})
+});
 
 module.exports = router;
