5575d1f905b4f34d206c4df0a6144b3bce093040
Ungs <50164778+iqeq1945@users.noreply.github.com>
2021-03-28 15:11:46 +0900

    # API 호출
    외부 API 호출 후 데이터 가져오기

--- routes/vaccine.js
+++ routes/vaccine.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
-//var request = require('require');
+var request = require('request');
 
-//const url = 'http://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=1&perPage=10';
-//const key = 'tNoct9hJ2VkbcD8RcdNsB8eVlJRGVALc80SKwRJu0jikOI5VbSTAXim5O07jvbEk7wTGmescTrqLsRd5Xz6WOw%3D%3D';
+const url = 'http://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=1&perPage=10';
+const key = 'tNoct9hJ2VkbcD8RcdNsB8eVlJRGVALc80SKwRJu0jikOI5VbSTAXim5O07jvbEk7wTGmescTrqLsRd5Xz6WOw%3D%3D';
+const api_url = url+'&serviceKey='+key;
+
 
 
 router.get('/', function(req, res, next){
-    //console.log(url,key);
+    request(api_url,function(err,res,body){
+        console.log(body);
+    });
     res.render('index');
 });
 
