ea453dfd49f88c2140dafbfc5056c57402bbba26
Ungs <50164778+iqeq1945@users.noreply.github.com>
2021-04-04 15:21:03 +0900

    #3 API 호출2, JSON

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,8 @@
+// routes/home.js
 var express = require('express');
 var router = express.Router();
 var StatData = require('./vaccine');
+var CenterData = require('./vaccine');
 
 router.get('/', function(req, res, next){
     StatData((err, {stat}={})=>{
@@ -8,7 +10,20 @@ router.get('/', function(req, res, next){
             console.log("error!");
             return res.send({err});
         }
-        return res.render('index',{stat:stat});
+        const json = JSON.parse(stat);
+        return res.render('index',{stat:json});
+    })
+});
+
+router.get('/centers',function(req,res,next){
+    CenterData((err, {center}={})=>{
+        if(err){
+            console.log("error!");
+            return res.send({err});
+        }
+        const json = JSON.parse(center);
+        console.log(center);
+        return res.render('center',{center:json});
     })
 });
 
--- routes/vaccine.js
+++ routes/vaccine.js
@@ -1,7 +1,9 @@
+// routes/vaccine.js
+
 var request = require('request');
 
 const StatData = (callback) =>{
-    console.log("통계데이터!!!");
+    //console.log("통계데이터!!!");
 
     const url = 'http://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=1&perPage=10';
     const key = 'tNoct9hJ2VkbcD8RcdNsB8eVlJRGVALc80SKwRJu0jikOI5VbSTAXim5O07jvbEk7wTGmescTrqLsRd5Xz6WOw%3D%3D';
@@ -14,4 +16,19 @@ const StatData = (callback) =>{
     });
 }
 
+const CenterData = (callback) =>{
+    //console.log("센터데이터!!!");
+
+    const url = 'https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=10';
+    const key = 'tNoct9hJ2VkbcD8RcdNsB8eVlJRGVALc80SKwRJu0jikOI5VbSTAXim5O07jvbEk7wTGmescTrqLsRd5Xz6WOw%3D%3D';
+    const api_url = url+'&serviceKey='+key;
+
+    request(api_url,function(err,res,body){
+        callback(undefined,{
+            center:body
+        });
+    });
+}
+
 module.exports = StatData;
+module.exports = CenterData;
